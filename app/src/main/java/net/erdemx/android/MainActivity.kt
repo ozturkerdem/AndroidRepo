f2a56faaeee279b56e8ba7a57448e86ccaeb2269
Erdem X <gamze.yildirim@intertech.com.tr>
2018-01-20 16:12:40 +0300

    [master] Initialize project.

---
+++ app/src/main/java/net/erdemx/android/MainActivity.kt
@@ -0,0 +1,38 @@
+package net.erdemx.android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnKill.setOnClickListener { finish() }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
